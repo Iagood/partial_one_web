9713ce09b01f088654ac0914220638995fbdb7a9
Iago da Conceição Barbosa <iago.barbosa@ses.ba>
2021-09-15 14:24:04 -0300

    Realiza tarefas solicitadas

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+    <html>
+        <head></head>
+        <body>
+            <center>
+                <h4> Média aritmética </h4>
+                <form type="submit" method="post" action="regra.php">
+                    <label for="nota1">Informe a primeira nota</label>
+                    <input type="number" required min="0" max="10" step=".01" maxlength="2" name="nota1">
+                    <br>
+                    <br>
+                    <label for="nota2">Informe a segunda nota</label>
+                    <input type="number" required min="0" max="10" maxlength="2" step=".01" name="nota2">
+                    <br>
+                    <br>
+                    <label for="nota3">Informe a terceira nota</label>
+                    <input type="number" required min="0" max="10" maxlength="2" step=".01" name="nota3">
+                    <br>
+                    <br>
+                    <button>Calcular média</button>
+                </form>
+                <hr>
+                <h4> Validador de mês </h4>
+                <form type="submit" method="post" action="regra.php">
+                    <label for="valor_mes">Informe o valor</label>
+                    <input type="number" required min="0" max="12" maxlength="2" name="valor_mes">
+                    <br>
+                    <br>
+                    <label for="texto_mes">Informe do texto</label>
+                    <input type="text" required name="texto_mes">
+                    <br>
+                    <br>
+                    <button>Validar mês</button>
+                </form>
+                <hr>
+                <h4>Verificar naturais</h4>
+                <form type="submit" method="post" action="regra.php">
+                <label for="valor1">Informe o primeiro valor </label>
+                    <input type="number" required min="0" maxlength="2" name="valor1">
+                    <br>
+                    <br>
+                    <label for="valor2">Informe o segundo valor </label>
+                    <input type="number" required min="0" maxlength="2"  name="valor2">
+                    <br>
+                    <br>
+                    <button>Verificar</button>
+                </form>
+            </center>
+        </body>
+    </html>
---
+++ regra.php
@@ -0,0 +1,83 @@
+<?php
+
+if(isset($_POST['nota1'])) {
+
+    return logicaMediaAritmetica();
+
+} elseif (isset($_POST['valor_mes'])) {
+
+    return validarMes();
+}
+else {
+    return verificarInteiros();
+}
+
+/////// LÓGICA PARA MÉDIA ARITMÉTICA //////
+function logicaMediaAritmetica()
+{
+    $nota1 = $_POST['nota1'];
+    $nota2 = $_POST['nota2'];
+    $nota3 = $_POST['nota3'];
+
+    $media = ($nota1 +$nota2 + $nota3) / 3;
+    $media_formatada =  number_format($media, 2, ',', ' ');
+    echo "A média obtida pela notas foi: " . $media_formatada . "<br>";
+
+    if($media_formatada >= 7) {
+        echo "O aluno foi aprovado!";
+    }
+    else if ($media_formatada < 7 && $media_formatada >= 4) {
+        echo "O aluno está em recuperação! <br>";
+    }
+    else {
+        echo "O aluno está reprovado! <br>";
+    }
+    echo '<br> <a href="index.php"> Voltar </a>';
+}
+
+///////// LÓGICA PARA VALIDAR MÊS ////////
+function validarMes()
+{
+    $valorMes = $_POST['valor_mes'];
+    $textoMes = strtoupper($_POST['texto_mes']);
+    $mesCorreto = 'Mês incorreto!';
+    $arrayMes = [
+    1 => 'Janeiro',
+    2 => 'Fevereiro',
+    3 => 'Março',
+    4 =>'Abril',
+    5 =>'Maio',
+    6 =>'Junho',
+    7 =>'Julho',
+    8 => 'Agosto',
+    9 => 'Setembro',
+    10 => 'Outubro',
+    11 => 'Novembro',
+    12 => 'Dezembro'
+    ];
+
+    foreach($arrayMes as $key => $mes) {
+        if($key == $valorMes && strtoupper($mes) == $textoMes)
+            $mesCorreto = 'Mês correto!';
+    }
+    echo $mesCorreto;
+    echo '<br> <a href="index.php"> Voltar </a>';
+}
+
+/////// LÓGICA PARA VERIFICAR INTEIROS E DIVISÍVEIS PELO PRIMEIRO PARÂMETRO /////////
+function verificarInteiros()
+{
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+
+
+    for ($i = 1; $i <= $valor1 ; $i ++) {
+        if ($i % $valor2 == 0) {
+            echo $i . " - ";
+        }
+    }
+
+    echo '<br> <a href="index.php"> Voltar </a>';
+}
+
+?>
